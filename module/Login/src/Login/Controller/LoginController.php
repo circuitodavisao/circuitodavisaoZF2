4b0825dad1861af7b20582fd2789ab4ee6c0cb49
root <root@homologacao>
2016-03-23 14:21:32 -0400

    [MERGE]

--- module/Login/src/Login/Controller/LoginController.php
+++ module/Login/src/Login/Controller/LoginController.php
@@ -3,9 +3,15 @@
 namespace Login\Controller;
 
 use Doctrine\ORM\EntityManager;
+use Exception;
 use Login\Controller\Helper\Constantes;
+use Login\Controller\Helper\LoginORM;
 use Login\Form\LoginForm;
+use Login\Form\RecuperarAcessoForm;
 use Zend\Authentication\AuthenticationService;
+use Zend\Mail\Message;
+use Zend\Mail\Transport\Smtp;
+use Zend\Mail\Transport\SmtpOptions;
 use Zend\Mvc\Controller\AbstractActionController;
 
 /**
@@ -41,15 +47,19 @@ class LoginController extends AbstractActionController {
         /* Limpar mensagens */
         $this->flashMessenger()->clearCurrentMessages();
 
+        $classeMessagem = Constantes::$CLASS_HIDDEN;
+
         $formLogin = new LoginForm(Constantes::$LOGIN_FORM);
 
         $inputEmailDaRota = $this->params()->fromRoute(Constantes::$INPUT_EMAIL);
         if (!empty($inputEmailDaRota)) {
             $formLogin->get(Constantes::$INPUT_EMAIL)->setValue($inputEmailDaRota);
+            $classeMessagem = '';
         }
 
         return [
             Constantes::$FORM_LOGIN => $formLogin,
+            Constantes::$CLASS_HIDDEN => $classeMessagem,
         ];
     }
 
@@ -88,9 +98,6 @@ class LoginController extends AbstractActionController {
         } else {
             /* Autenticacao falhou */
 
-            /* Mensagem de erro */
-            $this->flashMessenger()->
-                    addErrorMessage(Constantes::$MENSAGEM_FALHA_LOGIN);
             /* Redirecionamento */
             return $this->forward()->dispatch(Constantes::$CONTROLLER_LOGIN, array(
                         Constantes::$ACTION => Constantes::$ACTION_INDEX,
@@ -105,6 +112,82 @@ class LoginController extends AbstractActionController {
      */
     public function acessoAction() {
         return [];
+    }
+
+    /**
+     * Função que direciona a tela de esqueceu senha
+     * GET /esqueceuSenha
+     */
+    public function esqueceuSenhaAction() {
+        $formRecuperarAcesso = new RecuperarAcessoForm(Constantes::$RECUPERAR_ACESSO_FORM);
+        return [
+            Constantes::$FORM_RECUPERAR_ACESSO => $formRecuperarAcesso,
+        ];
+    }
+
+    /**
+     * Função que tenta recuperar o acesso
+     * GET /acesso
+     */
+    public function recuperarAcessoAction() {
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            /* Helper Controller */
+            $loginORM = new LoginORM($this->getDoctrineORMEntityManager());
+
+            /* Dados da requisição POST */
+            $data = $request->getPost();
+
+            $formRecuperarAcesso = new RecuperarAcessoForm(Constantes::$RECUPERAR_ACESSO_FORM);
+            $formRecuperarAcesso->setData($data);
+
+            if ($formRecuperarAcesso->isValid()) {
+                echo "IS VALID <br />";
+            } else {
+                echo "IS NOT VALID <br />";
+            }
+
+            /* Verificar se existe pessoa por email informado */
+            $email = $data[Constantes::$ENTITY_PESSOA_EMAIL];
+            $pessoa = $loginORM->getPessoaORM()->encontrarPorEmail($email);
+
+            $mensagem = '';
+            /* Pessoa com email informado nao encontrada */
+            if (!$pessoa) {
+                $mensagem = 'Pessoa nao encontrada';
+            } else {
+                if (!$pessoa->verificarSeEstaAtivo()) {
+                    $mensagem = 'Pessoa inativada';
+                } else {
+                    $mensagem = 'Pessoa ok truta';
+                    try {
+                        $message = new Message();
+                        $message->setBody('This is the body');
+                        $message->setFrom('myemail@mydomain.com');
+                        $message->addTo('falecomleonardopereira@gmail.com');
+                        $message->setSubject('Test subject');
+
+                        $smtpOptions = new SmtpOptions(array(
+                            'name' => 'smtp.circuitodavisao.com.br',
+                            'host' => 'smtp.circuitodavisao.com.br',
+                            'port' => 587,
+                            'connection_class' => 'smtp',
+                            'connection_config' => array(
+                                'username' => 'leonardo@circuitodavisao.com.br',
+                                'password' => 'Leonardo142857',
+                                'ssl' => 'tls',
+                            ),
+                        ));
+
+                        $transport = new Smtp($smtpOptions);
+                        $transport->send($message);
+                    } catch (Exception $exc) {
+                        echo $exc->getTraceAsString();
+                    }
+                }
+            }
+        }
+        return ['mensagem' => $mensagem];
     }
 
     public function getDoctrineORMEntityManager() {
---
+++ module/Login/src/Login/Form/RecuperarAcessoForm.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Login\Form;
+
+use Login\Controller\Helper\Constantes;
+use Zend\Captcha\Dumb;
+use Zend\Captcha\Image;
+use Zend\Form\Element\Button;
+use Zend\Form\Element\Captcha;
+use Zend\Form\Element\Radio;
+use Zend\Form\Element\Submit;
+use Zend\Form\Element\Text;
+use Zend\Form\Form;
+
+/**
+ * Nome: LoginForm.php
+ * @author Leonardo Pereira Magalhães <falecomleonardopereira@gmail.com>
+ * Descricao: Formulario para recuperar acesso
+ */
+class RecuperarAcessoForm extends Form {
+
+    /**
+     * Contrutor
+     * @param String $name
+     */
+    public function __construct($name = null) {
+        parent::__construct($name);
+
+        /**
+         * Configuração do formulário
+         */
+        $this->setAttributes(array(
+            'method' => 'POST',
+            'class' => 'form-horizontal',
+        ));
+
+        /**
+         * Radio para saber o que o usuario quer
+         */
+        $this->add(
+                (new Radio())
+                        ->setName(Constantes::$INPUT_OPCAO)
+                        ->setAttributes([
+                            'id' => Constantes::$INPUT_OPCAO,
+                        ])
+                        ->setOptions([
+                            'value_options' => array(
+                                1 => Constantes::$TRADUCAO_ESQUECI_MINHA_SENHA,
+                                2 => Constantes::$TRADUCAO_ESQUECI_MEU_USUARIO,
+                            ),
+                        ])
+        );
+
+        /**
+         * Botao de cancelar
+         */
+        $this->add(
+                (new Button())
+                        ->setName(Constantes::$INPUT_BOTAO_CANCELAR)
+                        ->setLabel(Constantes::$TRADUCAO_CANCELAR)
+                        ->setAttributes([
+                            'id' => Constantes::$INPUT_BOTAO_CANCELAR,
+                            'onClick' => 'location.href=\'' . Constantes::$INDEX . '\'',
+                            'class' => 'btn btn-info',
+                        ])
+                        ->setValue(Constantes::$TRADUCAO_CANCELAR)
+        );
+        /**
+         * Botao de continuar
+         */
+        $this->add(
+                (new Button())
+                        ->setName(Constantes::$INPUT_BOTAO_CONTINUAR)
+                        ->setLabel(Constantes::$TRADUCAO_CONTINUAR)
+                        ->setAttributes([
+                            'id' => Constantes::$INPUT_BOTAO_CONTINUAR,
+                            'onClick' => 'location.href=\'#\'',
+                            'class' => 'btn btn-primary',
+                        ])
+                        ->setValue(Constantes::$TRADUCAO_CONTINUAR)
+        );
+        /**
+         * Email de acesso
+         * Elemento do tipo text
+         */
+        $this->add(
+                (new Text())
+                        ->setName(Constantes::$INPUT_EMAIL)
+                        ->setAttributes([
+                            'class' => 'gui-input',
+                            'id' => Constantes::$INPUT_EMAIL,
+                            'placeholder' => Constantes::$TRADUCAO_USUARIO_PLACEHOLDER,
+                            'onkeypress' => 'capsLock(event)',
+                        ])
+        );
+
+        /**
+         * Captcha de validacao de acesso
+         */
+        $captcha = new Image(array(
+            'expiration' => '300',
+            'wordlen' => '1',
+            'font' => 'public/fonts/arial.ttf',
+            'fontSize' => '20',
+            'imgDir' => 'public/captcha',
+            'imgUrl' => '/captcha'
+        ));
+        $this->add(
+                (new Captcha())
+                        ->setName(Constantes::$INPUT_CAPTCHA)
+                        ->setOptions(
+                                array(
+                                    'label' => 'Verification',
+                                    'captcha' => $captcha,
+                        ))
+        );
+
+        /**
+         * Botao enviar email
+         */
+        $this->add(
+                (new Submit())
+                        ->setName(Constantes::$INPUT_BOTAO_ENVIAR_EMAIL)
+                        ->setValue(Constantes::$TRADUCAO_ENVIAR_EMAIL)
+                        ->setAttributes([
+                            'id' => Constantes::$INPUT_BOTAO_ENVIAR_EMAIL,
+                            'class' => 'btn btn-success',
+                        ])
+        );
+
+        /**
+         * CPF do usuario
+         * Elemento do tipo text
+         */
+        $this->add(
+                (new Text())
+                        ->setName(Constantes::$INPUT_CPF)
+                        ->setAttributes([
+                            'class' => 'gui-input',
+                            'id' => Constantes::$INPUT_CPF,
+                            'placeholder' => '##',
+                        ])
+        );
+
+        /**
+         * Data Nascimento do usuario
+         * Elemento do tipo text
+         */
+        $this->add(
+                (new Text())
+                        ->setName(Constantes::$INPUT_DATA_NASCIMENTO)
+                        ->setAttributes([
+                            'class' => 'gui-input',
+                            'id' => Constantes::$INPUT_DATA_NASCIMENTO,
+                            'placeholder' => '##/##/####',
+                        ])
+        );
+
+        /**
+         * Botao verificar acesso
+         */
+        $this->add(
+                (new Submit())
+                        ->setName(Constantes::$INPUT_BOTAO_VERIFICAR_USUARIO)
+                        ->setValue(Constantes::$TRADUCAO_VERIFICAR_USUARIO)
+                        ->setAttributes([
+                            'id' => Constantes::$INPUT_BOTAO_VERIFICAR_USUARIO,
+                            'class' => 'btn btn-success',
+                        ])
+        );
+    }
+
+}
