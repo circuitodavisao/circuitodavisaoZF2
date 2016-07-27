dc26026395978aca88f4424562c9e8182ad61f98
mrbreakbeat <mrbreakbeat@gmail.com>
2016-07-27 16:42:53 -0300

    [LANÇAMENTO][VALIDACOES-FORMULARIOS] Omitindo campos do formulario de cadastro de sub-equipe

--- public/js/circuito_da_visao/ocultar-campos-formularios.js
+++ public/js/circuito_da_visao/ocultar-campos-formularios.js
@@ -2,4 +2,18 @@
  * Gerando filtro de campos
  */
 
+$(document).ready(function(){
+  $("#hidealuno").hide();
+  $("#hidedados").hide();
 
+  $("#campos_casado").click(function(){
+    $("#hidealuno").show();
+    $("#hidedados").hide();
+  });
+
+  $("#campos_solteiro").click(function(){
+    $("#hidealuno").hide();
+    $("#hidedados").show();
+  });
+
+});
