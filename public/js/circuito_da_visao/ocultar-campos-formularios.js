179cba857668d4ba9ca2c211f60790e79f87ec32
mrbreakbeat <mrbreakbeat@gmail.com>
2016-08-01 15:17:07 -0300

    [LANÇAMENTO]MERGE

--- public/js/circuito_da_visao/ocultar-campos-formularios.js
+++ public/js/circuito_da_visao/ocultar-campos-formularios.js
@@ -3,17 +3,17 @@
  */
 
 $(document).ready(function(){
-    $("#hidealuno").hide();
-    $("#hidedados").hide();
+    $("#ocultar_aluno").hide();
+    $("#ocultar_dados").hide();
 
     $("#campos_casado").click(function(){
-        $("#hidealuno").show();
-        $("#hidedados").hide();
+        $("#ocultar_aluno").show();
+        $("#ocultar_dados").hide();
     });
 
     $("#campos_solteiro").click(function(){
-        $("#hidealuno").hide();
-        $("#hidedados").show();
+        $("#ocultar_aluno").hide();
+        $("#ocultar_dados").show();
     });
   
   
@@ -22,10 +22,46 @@ $(document).ready(function(){
     var opSelected = $("#tipo option:selected").val();
 
     if(opSelected == 0 || opSelected == 1){
-        $("#hidecheckbox").hide();
+        $("#ocultar_checkbox").hide();
     }else{
-        $("#hidecheckbox").show();
+        $("#ocultar_checkbox").show();
     }
  
   });
 });
+
+
+function validar_cadastro_pessoa(CadastrarPessoaForm) {
+    var msg_erro = "";
+    var tipo = CadastrarPessoaForm.tipo.value;
+    
+    //NÃO SEI O QUE QUER DIZER TIPO
+    //Verificar se o campo [NOME] esta vazio
+    if (tipo == "A"){
+        if (CadastrarPessoaForm.nome.value == "") {
+            if (msg_erro == "") {
+                msg_erro = "Nome da Pessoa";
+            } else {
+                msg_erro += ", Nome da Pessoa";
+            }
+        }
+
+        if (CadastrarPessoaForm.telefone.value == "") {
+            if (msg_erro == "") {
+                msg_erro = "Telefone da Pessoa";
+            } else {
+                msg_erro += ", Telefone da Pessoa";
+            }
+        }
+    }
+    
+    //NÃO ENTENDI ESSE CÓDIGO :/
+    if (msg_erro == "") {
+        $('#botaoAlterar').css('display', 'none');
+        $('#loader').css('display', 'block');
+        f.submit();
+    } else {
+        alert("Preencha o(s) seguinte(s) campo(s): " + msg_erro);
+        return 'false';
+    }
+}
