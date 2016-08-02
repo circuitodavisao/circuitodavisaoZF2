a891f69ed58101fdcd2907d62a066f4027168596
mrbreakbeat <mrbreakbeat@gmail.com>
2016-08-02 16:16:42 -0300

    [LANÇAMENTO][Validação de Formularios] Aplicando validações padrão do tema

--- public/js/circuito_da_visao/ocultar-campos-formularios.js
+++ public/js/circuito_da_visao/ocultar-campos-formularios.js
@@ -29,39 +29,3 @@ $(document).ready(function(){
  
   });
 });
-
-
-function validar_cadastro_pessoa(CadastrarPessoaForm) {
-    var msg_erro = "";
-    var tipo = CadastrarPessoaForm.tipo.value;
-    
-    //NÃO SEI O QUE QUER DIZER TIPO
-    //Verificar se o campo [NOME] esta vazio
-    if (tipo == "A"){
-        if (CadastrarPessoaForm.nome.value == "") {
-            if (msg_erro == "") {
-                msg_erro = "Nome da Pessoa";
-            } else {
-                msg_erro += ", Nome da Pessoa";
-            }
-        }
-
-        if (CadastrarPessoaForm.telefone.value == "") {
-            if (msg_erro == "") {
-                msg_erro = "Telefone da Pessoa";
-            } else {
-                msg_erro += ", Telefone da Pessoa";
-            }
-        }
-    }
-    
-    //NÃO ENTENDI ESSE CÓDIGO :/
-    if (msg_erro == "") {
-        $('#botaoAlterar').css('display', 'none');
-        $('#loader').css('display', 'block');
-        f.submit();
-    } else {
-        alert("Preencha o(s) seguinte(s) campo(s): " + msg_erro);
-        return 'false';
-    }
-}
