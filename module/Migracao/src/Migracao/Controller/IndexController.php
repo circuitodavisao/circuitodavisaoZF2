1cc6a81fb58445be870e6e9b7c427f89e949328b
leonardo pereira magalhaes <leonardo@circuitodavisao.com.br>
2017-01-17 13:53:24 -0200

    [MIGRACAO] Modulo de migração basico 3

--- module/Migracao/src/Migracao/Controller/IndexController.php
+++ module/Migracao/src/Migracao/Controller/IndexController.php
@@ -3,6 +3,10 @@
 namespace Migracao\Controller;
 
 use Application\Controller\CircuitoController;
+use Application\Model\Entity\Entidade;
+use Application\Model\Entity\Grupo;
+use Application\Model\Entity\GrupoResponsavel;
+use Application\Model\ORM\RepositorioORM;
 use Doctrine\ORM\EntityManager;
 use Exception;
 use Zend\View\Model\ViewModel;
@@ -34,6 +38,30 @@ class IndexController extends CircuitoController {
         $view = new ViewModel();
         echo "abrindo a conexao";
         $this->abreConexao();
+
+        $queryigrejas = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_igreja_ursula');
+        $repositorioORM = new RepositorioORM($this->getDoctrineORMEntityManager());
+
+        while ($row = mysqli_fetch_array($queryigrejas)) {
+            $grupoIgreja = new Grupo();
+//            $repositorioORM->getGrupoORM()->persistir($grupoIgreja);
+            $entidadeNova = new Entidade();
+            $entidadeNova->setEntidadeTipo(
+                    $repositorioORM->getEntidadeTipoORM()->encontrarPorId(6)
+            );
+            $entidadeNova->setGrupo($grupoIgreja);
+            $entidadeNova->setNome($row[1]);
+//            $repositorioORM->getEntidadeORM()->persistir($entidadeNova);
+            
+            $grupoResposanvel = new GrupoResponsavel();
+            $grupoResposanvel->setGrupo($grupoIgreja);
+            $grupoResposanvel->setPessoa($repositorioORM->getPessoaORM()->encontrarPorId(1));
+//            $repositorioORM->getGrupoResponsavelORM()->persistir($grupoResposanvel);
+
+            echo '<br />igreja: ' . $row[1];
+        }
+
+
         return $view;
     }
 
