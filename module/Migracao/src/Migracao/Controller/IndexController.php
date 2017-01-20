69dc8882a69f0653db6f4c8a98791aee3efd26ef
leonardo pereira magalhaes <leonardo@circuitodavisao.com.br>
2017-01-20 17:30:11 -0200

    [MIGRACAO] busca de lideres da igreja

--- module/Migracao/src/Migracao/Controller/IndexController.php
+++ module/Migracao/src/Migracao/Controller/IndexController.php
@@ -38,28 +38,44 @@ class IndexController extends CircuitoController {
         $view = new ViewModel();
         echo "abrindo a conexao";
         $this->abreConexao();
-
-        $queryigrejas = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_igreja_ursula');
         $repositorioORM = new RepositorioORM($this->getDoctrineORMEntityManager());
 
-        while ($row = mysqli_fetch_array($queryigrejas)) {
-            $grupoIgreja = new Grupo();
-//            $repositorioORM->getGrupoORM()->persistir($grupoIgreja);
-            $entidadeNova = new Entidade();
-            $entidadeNova->setEntidadeTipo(
-                    $repositorioORM->getEntidadeTipoORM()->encontrarPorId(6)
-            );
-            $entidadeNova->setGrupo($grupoIgreja);
-            $entidadeNova->setNome($row[1]);
-//            $repositorioORM->getEntidadeORM()->persistir($entidadeNova);
-            
-            $grupoResposanvel = new GrupoResponsavel();
-            $grupoResposanvel->setGrupo($grupoIgreja);
-            $grupoResposanvel->setPessoa($repositorioORM->getPessoaORM()->encontrarPorId(1));
-//            $repositorioORM->getGrupoResponsavelORM()->persistir($grupoResposanvel);
+        $queryIgrejas = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_igreja_ursula WHERE id = 1');
+        while ($row = mysqli_fetch_array($queryIgrejas)) {
+            $nomeIgreja = $row['nome'];
+            $queryLider1 = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_pessoa_ursula WHERE id = ' . $row['idResponsavel1']);
+            if ($row['idResponsavel2']) {
+                $queryLider2 = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_pessoa_ursula WHERE id = ' . $row['idResponsavel2']);
+            } 
 
-            echo '<br />igreja: ' . $row[1];
+            $queryEquipes = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_equipe_ursula WHERE idIgreja = 1');
+            while ($row = mysqli_fetch_array($queryEquipes)) {
+                $nomeEquipe = $row['nome'];
+
+                $querySubEquipes = mysqli_query($this->getConexao(), 'SELECT * FROM ursula_subequipe_ursula WHERE idEquipe = 1');
+                while ($row = mysqli_fetch_array($querySubEquipes)) {
+                    $numeroSubequipe = $row['numero'];
+                }
+            }
         }
+//        while ($row = mysqli_fetch_array($queryigrejas)) {
+//            $grupoIgreja = new Grupo();
+////            $repositorioORM->getGrupoORM()->persistir($grupoIgreja);
+//            $entidadeNova = new Entidade();
+//            $entidadeNova->setEntidadeTipo(
+//                    $repositorioORM->getEntidadeTipoORM()->encontrarPorId(6)
+//            );
+//            $entidadeNova->setGrupo($grupoIgreja);
+//            $entidadeNova->setNome($row[1]);
+////            $repositorioORM->getEntidadeORM()->persistir($entidadeNova);
+//            
+//            $grupoResposanvel = new GrupoResponsavel();
+//            $grupoResposanvel->setGrupo($grupoIgreja);
+//            $grupoResposanvel->setPessoa($repositorioORM->getPessoaORM()->encontrarPorId(1));
+////            $repositorioORM->getGrupoResponsavelORM()->persistir($grupoResposanvel);
+//
+//            echo '<br />igreja: ' . $row[1];
+//        }
 
 
         return $view;
