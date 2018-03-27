c3521b1bc194f98a92eedbdb076f788020eb87fb
homologacao <homologacao@homologacao-CV>
2018-03-27 16:12:51 -0300

    Celulas nao realizadas na pagina inicial 1.0

---
+++ implementarSistema.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Atualizando repositotio para instalar o docker
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# instalando o docker
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+# instalando sistema
+
+sudo docker run --name bdpg -e POSTGRES_PASSWORD=qwaszx159753 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -d postgres
+
+sudo cd /home/circuitodavisaoZF2
+
+sudo docker build -t cv:0.3 .
+sudo docker run -d --name cv --link bdpg:postgres -p 443:443 -v /home/circuitodavisaoZF2:/var/www/html cv:0.3
+
+# instalando o banco de dados
+
+sudo cat /home/circuitodavisaoZF2/dump/dump.sql | sudo docker exec -i bdpg psql -U postgres
