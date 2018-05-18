af70ba15e453d2862ddd194d70012f7790d3dcc9
homologacao <homologacao@homologacao-CV>
2018-05-18 15:51:30 -0300

    Cadastro de grupo ajuste

--- iniciarDocker.sh
+++ iniciarDocker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker start bdpg
 docker rm cv
-docker run -d --name cv --link bdpg:postgres -p 443:443 -v /home/circuitodavisaoZF2:/var/www/html cv:0.3
+docker run -d --name cv --link bdpg:postgres -p 443:443 -v /home/circuitodavisaoZF2:/var/www/html cv:0.5
