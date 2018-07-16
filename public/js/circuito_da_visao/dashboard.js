e2c9bf819ec93b505a9b41ca833481e25d62c8f0
homologacao <homologacao@homologacao-CV>
2018-07-16 14:10:28 -0300

    Dashboard com ajax 2.4

--- public/js/circuito_da_visao/dashboard.js
+++ public/js/circuito_da_visao/dashboard.js
@@ -7,34 +7,36 @@ function buscarDados() {
             /* Loader */
             $('#divProgressBar').css('width', '100%')
             $('#divProgressBar').html('100%')
-            $('#loader').addClass('hidden')
-            $('#divDados').removeClass('hidden')
-            /* Dados principais */
-            $('#spanNomeDeQuemEstaLogado').html(data.spanNomeDeQuemEstaLogado)
-            $('#fotoPerfil').attr('src', '/img/fotos/' + data.fotoPerfil)
-            $('#divModalDadosPrincipais').html(data.divModalDadosPrincipais)
-            $('#classePessoal').addClass('label-' + data.classePessoalCor)
-            $('#classePessoal').html(data.classePessoalString)
-            $('#classeEquipe').addClass('label-' + data.classeEquipeCor)
-            $('#classeEquipe').html(data.classeEquipeString)
-            /* Circuito me ajuda */
-            $('#divTabelaCircuitoMeAjuda').html(data.divTabelaCircuitoMeAjuda)
-            /* Barras de progressos */
-            $('#paragrafoBarraDeProgressoPessoalMembresia').addClass('text-' + data.corBarraDeProgressoPessoalMembresia)
-            $('#paragrafoBarraDeProgressoPessoalMembresia').html(data.fraseBarraDeProgressoPessoalMembresia)
-            $('#paragrafoBarraDeProgressoEquipeMembresia').addClass('text-' + data.corBarraDeProgressoEquipeMembresia)
-            $('#paragrafoBarraDeProgressoEquipeMembresia').html(data.fraseBarraDeProgressoEquipeMembresia)
-            $('#divBarraDeProgressoPessoalMembresia').html(data.divBarraDeProgressoPessoalMembresia)
-            $('#divBarraDeProgressoEquipeMembresia').html(data.divBarraDeProgressoEquipeMembresia)
-            $('#divDadosMembresia').html(data.divDadosMembresia)
-            $('#paragrafoBarraDeProgressoPessoalCelula').addClass('text-' + data.corBarraDeProgressoPessoalCelula)
-            $('#paragrafoBarraDeProgressoPessoalCelula').html(data.fraseBarraDeProgressoPessoalCelula)
-            $('#paragrafoBarraDeProgressoEquipeCelula').addClass('text-' + data.corBarraDeProgressoEquipeCelula)
-            $('#paragrafoBarraDeProgressoEquipeCelula').html(data.fraseBarraDeProgressoEquipeCelula)
-            $('#divBarraDeProgressoPessoalCelula').html(data.divBarraDeProgressoPessoalCelula)
-            $('#divBarraDeProgressoEquipeCelula').html(data.divBarraDeProgressoEquipeCelula)
-            $('#divDadosCelula').html(data.divDadosCelula)
-            $('#divDadosProximoNivel').html(data.divDadosProximoNivel)
+            setTimeout(() => {
+                $('#loader').addClass('hidden')
+                $('#divDados').removeClass('hidden')
+                /* Dados principais */
+                $('#spanNomeDeQuemEstaLogado').html(data.spanNomeDeQuemEstaLogado)
+                $('#fotoPerfil').attr('src', '/img/fotos/' + data.fotoPerfil)
+                $('#divModalDadosPrincipais').html(data.divModalDadosPrincipais)
+                $('#classePessoal').addClass('label-' + data.classePessoalCor)
+                $('#classePessoal').html(data.classePessoalString)
+                $('#classeEquipe').addClass('label-' + data.classeEquipeCor)
+                $('#classeEquipe').html(data.classeEquipeString)
+                /* Circuito me ajuda */
+                $('#divTabelaCircuitoMeAjuda').html(data.divTabelaCircuitoMeAjuda)
+                /* Barras de progressos */
+                $('#paragrafoBarraDeProgressoPessoalMembresia').addClass('text-' + data.corBarraDeProgressoPessoalMembresia)
+                $('#paragrafoBarraDeProgressoPessoalMembresia').html(data.fraseBarraDeProgressoPessoalMembresia)
+                $('#paragrafoBarraDeProgressoEquipeMembresia').addClass('text-' + data.corBarraDeProgressoEquipeMembresia)
+                $('#paragrafoBarraDeProgressoEquipeMembresia').html(data.fraseBarraDeProgressoEquipeMembresia)
+                $('#divBarraDeProgressoPessoalMembresia').html(data.divBarraDeProgressoPessoalMembresia)
+                $('#divBarraDeProgressoEquipeMembresia').html(data.divBarraDeProgressoEquipeMembresia)
+                $('#divDadosMembresia').html(data.divDadosMembresia)
+                $('#paragrafoBarraDeProgressoPessoalCelula').addClass('text-' + data.corBarraDeProgressoPessoalCelula)
+                $('#paragrafoBarraDeProgressoPessoalCelula').html(data.fraseBarraDeProgressoPessoalCelula)
+                $('#paragrafoBarraDeProgressoEquipeCelula').addClass('text-' + data.corBarraDeProgressoEquipeCelula)
+                $('#paragrafoBarraDeProgressoEquipeCelula').html(data.fraseBarraDeProgressoEquipeCelula)
+                $('#divBarraDeProgressoPessoalCelula').html(data.divBarraDeProgressoPessoalCelula)
+                $('#divBarraDeProgressoEquipeCelula').html(data.divBarraDeProgressoEquipeCelula)
+                $('#divDadosCelula').html(data.divDadosCelula)
+                $('#divDadosProximoNivel').html(data.divDadosProximoNivel)
+            }, 1000)
         }
     }
     , 'json');
@@ -46,7 +48,7 @@ function buscarDados() {
         $('#divProgressBar').css('width', '50%')
         $('#divProgressBar').html('50%')
     }, 2000);
-    
+
     for (let i = 51; i < 85; i++) {
         setTimeout(() => {
             $('#divProgressBar').css('width', i + '%')
