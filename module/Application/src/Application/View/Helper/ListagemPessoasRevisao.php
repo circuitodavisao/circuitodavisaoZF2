b1cfd813772e1350628044c28440155a49e4b14f
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-07-31 19:02:51 -0300

    Ajustes nos cusos antes da migração

--- module/Application/src/Application/View/Helper/ListagemPessoasRevisao.php
+++ module/Application/src/Application/View/Helper/ListagemPessoasRevisao.php
@@ -13,75 +13,75 @@ use Zend\View\Helper\AbstractHelper;
  */
 class ListagemPessoasRevisao extends AbstractHelper {
 
-    public function __construct() {
-        
-    }
+	public function __construct() {
 
-    public function __invoke() {
-        return $this->renderHtml();
-    }
+	}
 
-    public function renderHtml() {
-        $html = '';
-        $pessoas = array();
-        $frequencias = $this->view->evento->getEventoFrequencia();
-        if (($frequencias)) {
-            foreach ($frequencias as $frequencia) {
-                if ($frequencia->getFrequencia() == 'S') {
-                    $pessoas[] = $frequencia->getPessoa();
-                }
-            }
-        }
+	public function __invoke() {
+		return $this->renderHtml();
+	}
 
-        /* Sem pessoas cadastrados */
-        if (count($pessoas) == 0) {
-            $html .= '<div class="panel-body bg-light">';
-            $html .= '<div class="alert alert-warning"><i class="fa fa-warning pr10" aria-hidden="true"></i>&nbsp;Sem Fichas Ativas</div>';
-            $html .= '</div>';
-        } else {
-            $html .= '<div id="painelAlunos">';
-            $html .= $this->view->templateFormularioTopo('Selecione os Alunos que participarão da turma');
-            $html .= '<div class="panel-body bg-light">';
-            $html .= '<table class="table">';
-            $html .= '<thead>';
-            $html .= '<tr>';
-            $html .= '<th class="text-center">';
-            $html .= $this->view->translate(Constantes::$TRADUCAO_MATRICULA);
-            $html .= '</th>';
-            $html .= '<th class="text-center">';
-            $html .= $this->view->translate(Constantes::$TRADUCAO_NOME_REVISIONISTA);
-            $html .= '</th>';
-            $html .= '<td class="text-center"><input type="checkbox" onclick="marcarTodos(this);"/></th>';
-            $html .= '</td>';
-            $html .= '</thead>';
+	public function renderHtml() {
+		$html = '';
+		$pessoas = array();
+		$frequencias = $this->view->evento->getEventoFrequencia();
+		if (($frequencias)) {
+			foreach ($frequencias as $frequencia) {
+				if ($frequencia->getFrequencia() == 'S') {
+					$pessoas[] = $frequencia->getPessoa();
+				}
+			}
+		}
 
-            $html .= '<tbody>';
-            foreach ($pessoas as $pessoa) {
-                $html .= '<tr>';
-                $html .= '<td class="text-center">' . $pessoa->getId() . '</td>';
-                $html .= '<td class="text-center"><span class="visible-lg visible-md">' . $pessoa->getNome() . '</span><span class="visible-sm visible-xs">' . $pessoa->getNomePrimeiroUltimo() . '</span></td>';
-                $html .= '<td class="text-center"><input type="checkbox" name="alunos" id="' . $pessoa->getNome() . '" value="' . $pessoa->getId() . '"></td>';
-                $html .= '</tr>';
-            }
-            $html .= '</tbody>';
+		/* Sem pessoas cadastrados */
+		if (count($pessoas) == 0) {
+			$html .= '<div class="panel-body bg-light">';
+			$html .= '<div class="alert alert-warning"><i class="fa fa-warning pr10" aria-hidden="true"></i>&nbsp;Sem Fichas Ativas</div>';
+			$html .= '</div>';
+		} else {
+			$html .= '<div id="painelAlunos">';
+			$html .= $this->view->templateFormularioTopo('Selecione os Alunos que participarão da turma');
+			$html .= '<div class="panel-body bg-light">';
+			$html .= '<table class="table">';
+			$html .= '<thead>';
+			$html .= '<tr>';
+			$html .= '<th class="text-center">';
+			$html .= $this->view->translate(Constantes::$TRADUCAO_MATRICULA);
+			$html .= '</th>';
+			$html .= '<th class="text-center">';
+			$html .= $this->view->translate(Constantes::$TRADUCAO_NOME_REVISIONISTA);
+			$html .= '</th>';
+			$html .= '<td class="text-center"><input type="checkbox" onclick="marcarTodos(this);"/></th>';
+			$html .= '</td>';
+			$html .= '</thead>';
 
-            $html .= '</table>';
-            $html .= '</div>';
-            /* Fim panel-body */
-            $html .= '<div class="panel-footer text-right">';
-            $stringNomeDaFuncaoOnClickVoltar = 'funcaoCircuito("' . Constantes::$ROUTE_CADASTRO . Constantes::$PAGINA_LISTAGEM_REVISAO_TURMA . '", ' . $pessoa->getId() . ')';
-            $html .= $this->view->botaoLink($this->view->translate(Constantes::$TRADUCAO_VOLTAR), Constantes::$STRING_HASHTAG, 2, $this->view->funcaoOnClick($stringNomeDaFuncaoOnClickVoltar));
-            $stringNomeDaFuncaoOnClickProsseguir = 'mostrarResumo()';
-            $html .= $this->view->botaoLink($this->view->translate(Constantes::$TRADUCAO_CONFIRMACAO), Constantes::$STRING_HASHTAG, 0, $this->view->funcaoOnClick($stringNomeDaFuncaoOnClickProsseguir));
-            /* Fim Botões */
-            $html .= '</div>';
+			$html .= '<tbody>';
+			foreach ($pessoas as $pessoa) {
+				$html .= '<tr>';
+				$html .= '<td class="text-center">' . $pessoa->getId() . '</td>';
+				$html .= '<td class="text-center"><span class="visible-lg visible-md">' . $pessoa->getNome() . '</span><span class="visible-sm visible-xs">' . $pessoa->getNomePrimeiroUltimo() . '</span></td>';
+				$html .= '<td class="text-center"><input type="checkbox" name="aluno'.$pessoa->getId().'" id="' . $pessoa->getNome() . '" value="' . $pessoa->getId() . '"></td>';
+				$html .= '</tr>';
+			}
+			$html .= '</tbody>';
 
-            /* Fim panel-footer */
-            $html .= $this->view->templateFormularioRodape();
-            $html .= '</div>';
-        }
+			$html .= '</table>';
+			$html .= '</div>';
+			/* Fim panel-body */
+			$html .= '<div class="panel-footer text-right">';
+			$stringNomeDaFuncaoOnClickVoltar = 'funcaoCircuito("' . Constantes::$ROUTE_CADASTRO . Constantes::$PAGINA_LISTAGEM_REVISAO_TURMA . '", ' . $pessoa->getId() . ')';
+			$html .= $this->view->botaoLink($this->view->translate(Constantes::$TRADUCAO_VOLTAR), Constantes::$STRING_HASHTAG, 2, $this->view->funcaoOnClick($stringNomeDaFuncaoOnClickVoltar));
+			$stringNomeDaFuncaoOnClickProsseguir = 'mostrarResumo()';
+			$html .= $this->view->botaoLink($this->view->translate(Constantes::$TRADUCAO_CONFIRMACAO), Constantes::$STRING_HASHTAG, 0, $this->view->funcaoOnClick($stringNomeDaFuncaoOnClickProsseguir));
+			/* Fim Botões */
+			$html .= '</div>';
 
-        return $html;
-    }
+			/* Fim panel-footer */
+			$html .= $this->view->templateFormularioRodape();
+			$html .= '</div>';
+		}
+
+		return $html;
+	}
 
 }
