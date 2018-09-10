233b5fdfff0b4bea4c5bd8929c5b6460c5ab09ed
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-09-10 18:47:29 -0300

    ajuste solicitacoes

--- module/Application/src/Application/Controller/CadastroController.php
+++ module/Application/src/Application/Controller/CadastroController.php
@@ -2013,20 +2013,18 @@ class CadastroController extends CircuitoController {
 		$solicitacoes = $grupoIgreja->getSolicitacao();
 		$solicitacoesDivididasPorTipo = array();
 		foreach($solicitacoes as $solicitacao){
-			if($solicitacao->getSolicitacaoSituacaoAtiva()->getSituacao()->getId() !== Situacao::CONCLUIDO){
-				$adicionar = true;
-				if($entidade->getEntidadeTipo()->getId() === EntidadeTipo::equipe){
-					$objeto = $solicitacao->getObjeto1();
-					$grupo = $this->getRepositorio()->getGrupoORM()->encontrarPorId($objeto);
-					if($grupo->getGrupoEquipe()){
-						if($entidade->getGrupo()->getId() !== $grupo->getGrupoEquipe()->getId()){
-							$adicionar = false;	
-						}
+			$adicionar = true;
+			if($entidade->getEntidadeTipo()->getId() === EntidadeTipo::equipe){
+				$objeto = $solicitacao->getObjeto1();
+				$grupo = $this->getRepositorio()->getGrupoORM()->encontrarPorId($objeto);
+				if($grupo->getGrupoEquipe()){
+					if($entidade->getGrupo()->getId() !== $grupo->getGrupoEquipe()->getId()){
+						$adicionar = false;	
 					}
 				}
-				if($adicionar){
-					$solicitacoesDivididasPorTipo[$solicitacao->getSolicitacaoTipo()->getId()][] = $solicitacao;
-				}
+			}
+			if($adicionar){
+				$solicitacoesDivididasPorTipo[$solicitacao->getSolicitacaoTipo()->getId()][] = $solicitacao;
 			}
 		}
 		$solicitacoesTipo = $this->getRepositorio()->getSolicitacaoTipoORM()->encontrarTodos();
