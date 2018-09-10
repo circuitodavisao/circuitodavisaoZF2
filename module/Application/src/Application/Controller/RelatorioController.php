ae70d0fa44043c24ac7eef0c2e80d367835bcb93
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-09-10 16:03:30 -0300

    Relatorio de celulas nao realizadas no periodo

--- module/Application/src/Application/Controller/RelatorioController.php
+++ module/Application/src/Application/Controller/RelatorioController.php
@@ -591,12 +591,15 @@ class RelatorioController extends CircuitoController {
 	const dadosPessoais = 0;
 	const parceiroDeDeusValor = 12;
 
-	public static function relatorioCompleto($repositorio, $grupo, $tipoRelatorio, $mes, $ano, $tudo = true, $somado = false) {
+	public static function relatorioCompleto($repositorio, $grupo, $tipoRelatorio, $mes, $ano, $tudo = true, $somado = false, $periodo = 0) {
 		$relatorio = array();
 		$todosFilhos = array();
 		$arrayPeriodoDoMes = Funcoes::encontrarPeriodoDeUmMesPorMesEAno($mes, $ano);
+		if($periodo > 0){
+			$arrayPeriodoDoMes[0] = $periodo;
+			$arrayPeriodoDoMes[1] = $periodo;
+		}
 		$diferencaDePeriodos = self::diferencaDePeriodos($arrayPeriodoDoMes[0], $arrayPeriodoDoMes[1]);
-
 		if($tudo){
 			for ($indiceDeArrays = $arrayPeriodoDoMes[0]; $indiceDeArrays <= $arrayPeriodoDoMes[1]; $indiceDeArrays++) {
 				$grupoPaiFilhoFilhos = $grupo->getGrupoPaiFilhoFilhosAtivos($indiceDeArrays);
@@ -1641,4 +1644,138 @@ class RelatorioController extends CircuitoController {
 
 		return new ViewModel($dados);
 	}
+
+	public function celulasNaoRealizadasAction(){
+		$sessao = new Container(Constantes::$NOME_APLICACAO);
+
+		$idEntidadeAtual = $sessao->idEntidadeAtual;
+		$entidade = $this->getRepositorio()->getEntidadeORM()->encontrarPorId($idEntidadeAtual);
+		$grupo = $entidade->getGrupo();
+
+		$periodo = -1;
+		$relatorios = array();
+
+		$relatorioCelulas =	self::pegarCelulasNaoRealizadasNoPeriodo($this->getRepositorio(), $grupo, $periodo);
+		foreach($relatorioCelulas as $chave => $valor){
+			$dados = array(
+				'idGrupoEquipe' => 0,
+				'idGrupoEvento' => $chave, 
+				'realizada' => $valor['realizada'],
+			);
+			$relatorios[] = $dados;
+		}
+
+		$periodoAfrente = 1;
+		$grupoPaiFilhoFilhos144 = $grupo->getGrupoPaiFilhoFilhosAtivos($periodoAfrente);
+		if ($grupoPaiFilhoFilhos144) {
+			foreach ($grupoPaiFilhoFilhos144 as $gpFilho144) {
+				$grupoFilho144 = $gpFilho144->getGrupoPaiFilhoFilho();
+				$valorSub = 0;
+				if($grupo->getEntidadeAtiva()->getEntidadeTipo()->getId() === EntidadeTipo::igreja){
+					$valorSub = $grupoFilho144->getEntidadeAtiva()->getNome();
+				}
+				if($grupo->getEntidadeAtiva()->getEntidadeTipo()->getId() === EntidadeTipo::equipe ||
+					$grupo->getEntidadeAtiva()->getEntidadeTipo()->getId() === EntidadeTipo::subEquipe){
+						$valorSub = $grupoFilho144->getEntidadeAtiva()->getNumero();
+					}
+				$relatorioCelulas =	self::pegarCelulasNaoRealizadasNoPeriodo($this->getRepositorio(), $grupoFilho144, $periodo);
+				foreach($relatorioCelulas as $chave => $valor){
+					$dados = array(
+						'idGrupoEquipe' => $valorSub,
+						'idGrupoEvento' => $chave, 
+						'realizada' => $valor['realizada'],
+					);
+					$relatorios[] = $dados;
+				}
+
+				$grupoPaiFilhoFilhos1728 = $grupoFilho144->getGrupoPaiFilhoFilhosAtivos($periodoAfrente);
+				if ($grupoPaiFilhoFilhos1728) {
+					foreach ($grupoPaiFilhoFilhos1728 as $gpFilho1728) {
+						$grupoFilho1728 = $gpFilho1728->getGrupoPaiFilhoFilho();
+						$relatorioCelulas =	self::pegarCelulasNaoRealizadasNoPeriodo($this->getRepositorio(), $grupoFilho1728, $periodo);
+						foreach($relatorioCelulas as $chave => $valor){
+							$dados = array(
+								'idGrupoEquipe' => $valorSub,
+								'idGrupoEvento' => $chave, 
+								'realizada' => $valor['realizada'],
+							);
+							$relatorios[] = $dados;
+						}				
+
+						$grupoPaiFilhoFilhos20736 = $grupoFilho1728->getGrupoPaiFilhoFilhosAtivos($periodoAfrente);
+						if ($grupoPaiFilhoFilhos20736) {
+							foreach ($grupoPaiFilhoFilhos20736 as $gpFilho20736) {
+								$grupoFilho20736 = $gpFilho20736->getGrupoPaiFilhoFilho();
+								$relatorioCelulas =	self::pegarCelulasNaoRealizadasNoPeriodo($this->getRepositorio(), $grupoFilho20736, $periodo);
+								foreach($relatorioCelulas as $chave => $valor){
+									$dados = array(
+										'idGrupoEquipe' => $valorSub,
+										'idGrupoEvento' => $chave, 
+										'realizada' => $valor['realizada'],
+									);
+									$relatorios[] = $dados;
+
+									$grupoPaiFilhoFilhos248832 = $grupoFilho20736->getGrupoPaiFilhoFilhosAtivos($periodoAfrente);
+									if ($grupoPaiFilhoFilhos248832) {
+										foreach ($grupoPaiFilhoFilhos248832 as $gpFilho248832) {
+											$grupoFilho248832 = $gpFilho248832->getGrupoPaiFilhoFilho();
+											$relatorioCelulas =	self::pegarCelulasNaoRealizadasNoPeriodo($this->getRepositorio(), $grupoFilho248832, $periodo);
+											foreach($relatorioCelulas as $chave => $valor){
+												$dados = array(
+													'idGrupoEquipe' => $valorSub,
+													'idGrupoEvento' => $chave, 
+													'realizada' => $valor['realizada'],
+												);
+												$relatorios[] = $dados;
+											}	
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		$tamanhoDoArray = count($relatorios);
+
+		for($i = 0; $i < $tamanhoDoArray; $i++){
+			for($j = 0; $j < $tamanhoDoArray; $j++){
+				$posicao1 = $relatorios[$i];
+				$posicao2 = $relatorios[$j];
+
+				if($posicao1['idGrupoEquipe'] < $posicao2['idGrupoEquipe']){
+					$auxiliar = $posicao1;
+					$relatorios[$i] = $posicao2;
+					$relatorios[$j] = $auxiliar;
+				}
+			}
+		}
+
+		return new ViewModel(
+			array(
+				'relatorio' => $relatorios,
+				'repositorio' => $this->getRepositorio(),
+				'periodo' => $periodo,
+			));
+	}
+
+	public static function pegarCelulasNaoRealizadasNoPeriodo(RepositorioORM $repositorioORM, Grupo $grupo, $periodo) {
+		$relatorio = array();
+		$grupoEventosCelula = $grupo->getGrupoEventoAtivosPorTipo(EventoTipo::tipoCelula);
+
+		foreach ($grupoEventosCelula as $grupoEventoCelula) {
+			$eventoId = $grupoEventoCelula->getEvento()->getId();
+			$resultado = $repositorioORM->getFatoCicloORM()->verificaFrequenciasPorCelulaEPeriodo($periodo, $eventoId);
+			$realizada = false;
+			if($resultado > 0){
+				$realizada = true;
+			}
+			$relatorio[$grupoEventoCelula->getId()]['realizada'] = $realizada;
+		}
+		return $relatorio;
+	}
+
+
 }
