592bb588c9e092f7fce08edfe4603eb61afc8878
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-09-19 18:23:08 -0300

    ajustes nas tranferencia e ajuste nas passadas

--- module/Application/src/Application/Model/ORM/FatoLiderORM.php
+++ module/Application/src/Application/Model/ORM/FatoLiderORM.php
@@ -15,19 +15,19 @@ use Exception;
  */
 class FatoLiderORM extends CircuitoORM {
 
-    /**
-     * Localizar fato_lider por numeroIdentificador
-     * @param String $numeroIdentificador
-     * @param integer $tipoComparacao
-     * @return String
-     */
-    public function encontrarPorNumeroIdentificador($numeroIdentificador, $tipoComparacao, $periodo = 0, $inativo = false) {
-        $dqlBase = "SELECT "
-                . "SUM(fl.lideres) lideres "
-                . "FROM  " . Constantes::$ENTITY_FATO_LIDER . " fl "
-                . "WHERE "
-				. "fl.numero_identificador #tipoComparacao ?1 "
-				. "AND ((fl.data_criacao <= ?2 AND fl.data_inativacao IS NULL) OR (#inativo)) ";
+	/**
+	 * Localizar fato_lider por numeroIdentificador
+	 * @param String $numeroIdentificador
+	 * @param integer $tipoComparacao
+	 * @return String
+	 */
+	public function encontrarPorNumeroIdentificador($numeroIdentificador, $tipoComparacao, $periodo = 0, $inativo = false) {
+		$dqlBase = "SELECT "
+			. "SUM(fl.lideres) lideres "
+			. "FROM  " . Constantes::$ENTITY_FATO_LIDER . " fl "
+			. "WHERE "
+			. "fl.numero_identificador #tipoComparacao ?1 "
+			. "AND ((fl.data_criacao <= ?2 AND fl.data_inativacao IS NULL) OR (#inativo)) ";
 
 		$resultadoPeriodo = Funcoes::montaPeriodo($periodo);
 		$dataDoPeriodo = $resultadoPeriodo[3] . '-' . $resultadoPeriodo[2] . '-' . $resultadoPeriodo[1];
@@ -46,62 +46,79 @@ class FatoLiderORM extends CircuitoORM {
 			$result = $this->getEntityManager()->createQuery($dqlAjustadaTipoComparacao)
 				->setParameter(1, $numeroIdentificador)
 				->setParameter(2, $dataDoPeriodoFormatada)
-                    ->getResult();
-            return $result;
-        } catch (Exception $exc) {
-            echo $exc->getMessage();
-        }
-    }
+				->getResult();
+			return $result;
+		} catch (Exception $exc) {
+			echo $exc->getMessage();
+		}
+	}
 
-    public function encontrarFatoLiderPorNumeroIdentificador($numeroIdentificador) {
-        $resposta = null;
-        try {
-            $entidade = $this->getEntityManager()
-                    ->getRepository($this->getEntity())
-                    ->findOneBy(array(Constantes::$ENTITY_FATO_CICLO_NUMERO_IDENTIFICADOR => $numeroIdentificador));
-            if ($entidade) {
-                $resposta = $entidade;
-            }
-            return $resposta;
-        } catch (Exception $exc) {
-            echo $exc->getTraceAsString();
-        }
-    }
-    
-     public function encontrarVariosFatoLiderPorNumeroIdentificador($numeroIdentificador) {
-        $resposta = null;
-        try {
-            $entidade = $this->getEntityManager()
-                    ->getRepository($this->getEntity())
-                    ->findBy(array(Constantes::$ENTITY_FATO_CICLO_NUMERO_IDENTIFICADOR => $numeroIdentificador));
-            if ($entidade) {
-                $resposta = $entidade;
-            }
-            return $resposta;
-        } catch (Exception $exc) {
-            echo $exc->getTraceAsString();
-        }
-    }
+	public function encontrarFatoLiderPorNumeroIdentificador($numeroIdentificador) {
+		$resposta = null;
+		try {
+			$entidade = $this->getEntityManager()
+				->getRepository($this->getEntity())
+				->findOneBy(array(Constantes::$ENTITY_FATO_CICLO_NUMERO_IDENTIFICADOR => $numeroIdentificador));
+			if ($entidade) {
+				$resposta = $entidade;
+			}
+			return $resposta;
+		} catch (Exception $exc) {
+			echo $exc->getTraceAsString();
+		}
+	}
 
-    /**
-     * Criar fato lider
-     * @param String $numeroIdentificador
-     * @param integer $quantidadeDeLideres
-     */
-    public function criarFatoLider($numeroIdentificador, $quantidadeDeLideres, $dataCriacao = null) {
-        $fatoLider = new FatoLider();
-        $alterarDiaDeCriacao = true;
-        if ($dataCriacao) {
-            $fatoLider->setDataEHoraDeCriacao($dataCriacao);
-            $alterarDiaDeCriacao = false;
-        }
-        try {
-            $fatoLider->setNumero_identificador($numeroIdentificador);
-            $fatoLider->setLideres($quantidadeDeLideres);
-            $this->persistir($fatoLider, $alterarDiaDeCriacao);
-        } catch (Exception $exc) {
-            echo $exc->getMessage();
-        }
-    }
+	public function encontrarFatoLiderPorNumeroGrupo($numeroIdentificador) {
+		$dql = "SELECT fl "
+			. "FROM  " . Constantes::$ENTITY_FATO_LIDER . " fl "
+			. "WHERE "
+			. "fl.numero_identificador LIKE ?1 ";
+
+		try {
+			$numeroIdentificador = '%'.$numeroIdentificador;
+			$result = $this->getEntityManager()->createQuery($dql)
+				->setParameter(1, $numeroIdentificador)
+				->getResult();
+			return $result;
+		} catch (Exception $exc) {
+			echo $exc->getTraceAsString();
+		}
+	}
+
+	public function encontrarVariosFatoLiderPorNumeroIdentificador($numeroIdentificador) {
+		$resposta = null;
+		try {
+			$entidade = $this->getEntityManager()
+				->getRepository($this->getEntity())
+				->findBy(array(Constantes::$ENTITY_FATO_CICLO_NUMERO_IDENTIFICADOR => $numeroIdentificador));
+			if ($entidade) {
+				$resposta = $entidade;
+			}
+			return $resposta;
+		} catch (Exception $exc) {
+			echo $exc->getTraceAsString();
+		}
+	}
+
+	/**
+	 * Criar fato lider
+	 * @param String $numeroIdentificador
+	 * @param integer $quantidadeDeLideres
+	 */
+	public function criarFatoLider($numeroIdentificador, $quantidadeDeLideres, $dataCriacao = null) {
+		$fatoLider = new FatoLider();
+		$alterarDiaDeCriacao = true;
+		if ($dataCriacao) {
+			$fatoLider->setDataEHoraDeCriacao($dataCriacao);
+			$alterarDiaDeCriacao = false;
+		}
+		try {
+			$fatoLider->setNumero_identificador($numeroIdentificador);
+			$fatoLider->setLideres($quantidadeDeLideres);
+			$this->persistir($fatoLider, $alterarDiaDeCriacao);
+		} catch (Exception $exc) {
+			echo $exc->getMessage();
+		}
+	}
 
 }
