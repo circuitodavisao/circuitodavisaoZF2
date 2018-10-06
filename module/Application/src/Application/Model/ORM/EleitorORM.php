b673561f6fbaa72dedb73f9e0e3b5b4b9a41b64b
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-10-06 01:54:29 -0300

    lista de 100 em 100 do tipo 2

--- module/Application/src/Application/Model/ORM/EleitorORM.php
+++ module/Application/src/Application/Model/ORM/EleitorORM.php
@@ -8,15 +8,16 @@ use Application\Model\ORM\CircuitoORM;
 
 class EleitorORM extends CircuitoORM {
 
-	public function encontrarPorBloco($bloco) {
+	public function encontrarPorBloco($bloco, $lista = 2) {
 		$entidades = null;
 		try {
 			$dql = "SELECT e.id, e.telefone, e.situacao, e.lista "
-				. "FROM  " . Constantes::$ENTITY_ELEITOR . " e ORDER BY e.id ASC ";
+				. "FROM  " . Constantes::$ENTITY_ELEITOR . " e WHERE e.lista = ?1 ORDER BY e.id ASC ";
 
-			$offset = $bloco * 500;
+			$offset = $bloco * 100;
 			$entidades = $this->getEntityManager()->createQuery($dql)
-				->setMaxResults(500)
+				->setParameter(1, $lista)
+				->setMaxResults(100)
 				->setFirstResult($offset)
 				->getResult();
 		} catch (Exception $exc) {
@@ -40,9 +41,10 @@ class EleitorORM extends CircuitoORM {
 		return $entidades;
 	}
 
-	public function totalDeRegistros(){
-		$dql = " SELECT count(e.id) FROM ". Constantes::$ENTITY_ELEITOR . " e ";
+	public function totalDeRegistros($lista = 2){
+		$dql = " SELECT count(e.id) FROM ". Constantes::$ENTITY_ELEITOR . " e WHERE e.lista = ?1 ";
 		$resultado = $this->getEntityManager()->createQuery($dql)
+				->setParameter(1, $lista)
 			->getResult();
 		return $resultado[0][1];
 	}
