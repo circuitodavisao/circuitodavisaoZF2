8822ecf748bbad3b5c814b6005e724ef32792d67
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2018-10-18 16:44:44 -0300

    manter revisao de vidas e some quando ja passar

--- module/Application/src/Application/Form/RevisaoForm.php
+++ module/Application/src/Application/Form/RevisaoForm.php
@@ -3,107 +3,112 @@
 namespace Application\Form;
 
 use Application\Controller\Helper\Constantes;
-use Application\Controller\Helper\Funcoes;
 use Application\Model\Entity\Evento;
-use Application\Model\ORM\RepositorioORM;
-use Zend\Form\Element\Date;
+use Zend\Form\Element\Csrf;
 use Zend\Form\Element\Hidden;
-use Zend\Form\Element\MultiCheckbox;
+use Zend\Form\Element\Select;
 use Zend\Form\Element\Text;
 use Zend\Form\Form;
 
 /**
  * Nome: RevisaoForm.php
- * @author Lucas Carvalho  <lucascarvalho.esw@gmail.com>
- * Descricao: Formulario para cadastrar revisõ.            
- *              
+ * @author Leonardo Pereira Magalhães <falecomleonardopereira@gmail.com>
+ * Descricao: Formulario para cadastrar revisao
  */
 class RevisaoForm extends Form {
 
-    /**
-     * Contrutor
-     * @param String $name
-     */
-    public function __construct($name = null, $gruposIgrejas = null, Evento $revisao = null) {
-        parent::__construct($name);
-        /**
-         * Configuração do formulário
-         */
-        $this->setAttributes(array(
-            Constantes::$FORM_STRING_METHOD => Constantes::$FORM_STRING_POST,
-        ));
-        /**
-         * Id
-         * Elemento do tipo text
-         */
-        $this->add(
-                (new Hidden())
-                        ->setName(Constantes::$ID)
-                        ->setAttributes([
-                            Constantes::$FORM_STRING_ID => Constantes::$ID,
-                        ])
-        );
+	/**
+	 * Contrutor
+	 * @param String $name
+	 * @param GrupoAluno $alunos
+	 */
+	public function __construct($name = null, Evento $evento = null) {
+		parent::__construct($name);
 
+		/**
+		 * Configuração do formulário
+		 */
+		$this->setAttributes(array(
+			Constantes::$FORM_STRING_METHOD => Constantes::$FORM_STRING_POST,
+			Constantes::$FORM_ACTION => '/cadastroRevisaoFinalizar',
+		));
 
-        /**
-         * Dia do Revisao
-         * Elemento do tipo Date
-         */
-        $this->add(
-                (new Date())
-                        ->setName(Constantes::$FORM_INPUT_DATA_REVISAO)
-                        ->setAttributes([
-                            Constantes::$FORM_STRING_CLASS => Constantes::$FORM_CLASS_FORM_CONTROL,
-                            Constantes::$FORM_STRING_ID => Constantes::$FORM_INPUT_DATA_REVISAO,
-                        ])
-        );
+		$this->add(
+			(new Hidden())
+			->setName(Constantes::$FORM_ID)
+		);
 
+		$this->add(
+			(new Csrf())
+			->setName(Constantes::$INPUT_CSRF)
+		);
 
-        /* Observacao */
-        $this->add(
-                (new Text())
-                        ->setName(Constantes::$FORM_NOME)
-                        ->setAttributes([
-                            Constantes::$FORM_CLASS => Constantes::$FORM_CLASS_GUI_INPUT,
-                            Constantes::$FORM_ID => Constantes::$FORM_NOME,
-                            Constantes::$FORM_PLACEHOLDER => Constantes::$TRADUCAO_NOME,
-                        ])
-        );
+		/* Dia da data de nascimento */
+		$arrayDiaDataNascimento = array();
+		$arrayDiaDataNascimento[0] = Constantes::$TRADUCAO_DIA;
+		for ($indiceDiaDoMes = 1; $indiceDiaDoMes <= 31; $indiceDiaDoMes++) {
+			$numeroAjustado = str_pad($indiceDiaDoMes, 2, 0, STR_PAD_LEFT);
+			$arrayDiaDataNascimento[$indiceDiaDoMes] = $numeroAjustado;
+		}
+		$inputSelectDiaDataNascimento = new Select();
+		$inputSelectDiaDataNascimento->setName(Constantes::$FORM_INPUT_DIA);
+		$inputSelectDiaDataNascimento->setAttributes(array(
+			Constantes::$FORM_CLASS => Constantes::$FORM_CLASS_FORM_CONTROL,
+			Constantes::$FORM_ID => Constantes::$FORM_INPUT_DIA,
+		));
+		$inputSelectDiaDataNascimento->setValueOptions($arrayDiaDataNascimento);
+		$this->add($inputSelectDiaDataNascimento);
 
+		/* Mês da data de nascimento */
+		$arrayMesDataNascimento = array();
+		$arrayMesDataNascimento[0] = Constantes::$TRADUCAO_MES;
+		for ($indiceMesNoAno = 1; $indiceMesNoAno <= 12; $indiceMesNoAno++) {
+			$numeroAjustado = str_pad($indiceMesNoAno, 2, 0, STR_PAD_LEFT);
+			$arrayMesDataNascimento[$indiceMesNoAno] = $numeroAjustado;
+		}
+		$inputSelectMesDataNascimento = new Select();
+		$inputSelectMesDataNascimento->setName(Constantes::$FORM_INPUT_MES);
+		$inputSelectMesDataNascimento->setAttributes(array(
+			Constantes::$FORM_CLASS => Constantes::$FORM_CLASS_FORM_CONTROL,
+			Constantes::$FORM_ID => Constantes::$FORM_INPUT_MES,
+		));
+		$inputSelectMesDataNascimento->setValueOptions($arrayMesDataNascimento);
+		$this->add($inputSelectMesDataNascimento);
 
+		/* Ano da data de nascimento */
+		$arrayAnoDataNascimento = array();
+		$arrayAnoDataNascimento[0] = Constantes::$TRADUCAO_ANO;
+		$anoAtual = date('Y');
+		for ($indiceAno = $anoAtual; $indiceAno <= ($anoAtual + 1); $indiceAno++) {
+			$arrayAnoDataNascimento[$indiceAno] = $indiceAno;
+		}
+		$inputSelectAnoDataNascimento = new Select();
+		$inputSelectAnoDataNascimento->setName(Constantes::$FORM_INPUT_ANO);
+		$inputSelectAnoDataNascimento->setAttributes(array(
+			Constantes::$FORM_CLASS => Constantes::$FORM_CLASS_FORM_CONTROL,
+			Constantes::$FORM_ID => Constantes::$FORM_INPUT_ANO,
+		));
+		$inputSelectAnoDataNascimento->setValueOptions($arrayAnoDataNascimento);
+		$this->add($inputSelectAnoDataNascimento);
 
-        if (!is_null($revisao->getId())) {
+		$this->add(
+			(new Text())
+			->setName(Constantes::$FORM_NOME)
+			->setAttributes([
+				Constantes::$FORM_CLASS => Constantes::$FORM_CLASS_FORM_CONTROL,
+				Constantes::$FORM_ID => Constantes::$FORM_NOME,
+				Constantes::$FORM_PLACEHOLDER => 'Não Obrigatório',
+			])
+		);
 
-            $this->get(Constantes::$FORM_ID)->setValue($revisao->getId());
-            $this->get(Constantes::$FORM_INPUT_DATA_REVISAO)->setValue(Funcoes::mudarPadraoData($revisao->getData(), 1));
-            $this->get(Constantes::$FORM_NOME)->setValue($revisao->getNome());
-            foreach ($gruposIgrejas as $gi) {
-                $i = $gi->getEntidadeAtiva();
-                if ($gi->verificaSeParticipaDoEvento($this->get(Constantes::$FORM_ID)->getValue())) {
-                    $selected = true;
-                }
-
-                $arrayIgrejas [] = array(
-                    'value' => $i->getNome() . '#' . $i->getId(),
-                    'label' => $i->getNome(),
-                    'selected' => $selected,
-                );
-            }
-        } else {
-            foreach ($gruposIgrejas as $gi) {
-                $i = $gi->getEntidadeAtiva();
-                $arrayIgrejas [] = array(
-                    'value' => $i->getNome() . '#' . $i->getId(),
-                    'label' => $i->getNome(),
-                    'selected' => false,
-                );
-            }
-        }
-
-        $multiCheckbox = new MultiCheckbox('igrejas');
-        $multiCheckbox->setValueOptions($arrayIgrejas);
-        $multiCheckbox->setAttribute('id', 'igrejas');
-        $this->add($multiCheckbox);
-    }
+		if($evento){
+			$this->get(Constantes::$FORM_ID)->setValue($evento->getId());
+			$explodeData = explode('-', $evento->getData());
+			$this->get(Constantes::$FORM_INPUT_DIA)->setValue($explodeData[2]);
+			$this->get(Constantes::$FORM_INPUT_MES)->setValue($explodeData[1]);
+			$this->get(Constantes::$FORM_INPUT_ANO)->setValue($explodeData[0]);
+			$this->get(Constantes::$FORM_NOME)->setValue($evento->getNome());
+		}
+	}
 
 }
