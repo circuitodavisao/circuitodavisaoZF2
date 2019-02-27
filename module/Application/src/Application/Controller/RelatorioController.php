997b20487a3e8e2f9cbc8032b8db0000d9967449
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-02-27 17:17:46 -0300

    atualizacao nos registro de acao

--- module/Application/src/Application/Controller/RelatorioController.php
+++ module/Application/src/Application/Controller/RelatorioController.php
@@ -2402,6 +2402,8 @@ public function alunosNaSemanaAction(){
 		$dados = array();
 		$dados['registros'] = $registrosOrganizados;
 
+		self::registrarLog(RegistroAcao::VER_RELATORIO_DE_REGISTRO, $extra = '');
+
 		return new ViewModel($dados);
 	}
 
