9eba3be94fbfafea30fbf1d9c4e1c1c11589a655
LEONARDO PEREIRA <falecomleonardopereira@gmail.com>
2020-02-12 19:10:14 -0200

    timeout maior relatorio pessoas no revisao do ano

--- module/Application/src/Application/Controller/RelatorioController.php
+++ module/Application/src/Application/Controller/RelatorioController.php
@@ -432,6 +432,10 @@ class RelatorioController extends CircuitoController {
 	}
 
 	public function quantidadeDePessoasPorRevisaoAction(){
+		set_time_limit(0);
+		ini_set('memory_limit', '-1');
+		ini_set('max_execution_time', '180');
+
 		$dados = array();
 		$sessao = new Container(Constantes::$NOME_APLICACAO);
 		$idEntidadeAtual = $sessao->idEntidadeAtual;
