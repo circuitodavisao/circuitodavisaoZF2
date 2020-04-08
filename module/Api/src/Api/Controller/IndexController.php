9a554c6731353c5dbe1947d62d2c20dd0f6e1a16
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2020-04-08 17:50:09 -0300

    novo teste

--- module/Api/src/Api/Controller/IndexController.php
+++ module/Api/src/Api/Controller/IndexController.php
@@ -26,29 +26,75 @@ class IndexController extends AbstractRestfulController {
 		$response = $this->getResponse();
 		$request = $this->getRequest();
 
+		static $ESTADO_PAGO = 2;
+		static $ESTADO_AUTORIZADO_CARTAO_CREDITO = 7;
+
 		error_log('checkoutAction');
-
-		if ($request->isGet()) {
-			error_log('get');
-		}
-
-		if ($request->isPost()) {
-			error_log('post');
-		}
 
 		error_log(print_r($request->getPost(), true));
 
 		if ($request->isPost()) {
-
-			error_log('Json');
-			$data = Json::decode($request->getContent());
-			error_log(print_r($data, true));
-
 			error_log('post');
 			$dataPost = $request->getPost();
-			error_log(print_r($dataPost,true));
+			$email = $dataPost['customer_email'];
+			$produto_id = $dataPost['product_id'];
+			$estado_pagamento = $dataPost['payment_status'];
+			if(
+				intVal($estado_pagamento) === $ESTADO_PAGO ||
+				intVal($estado_pagamento) === $ESTADO_AUTORIZADO_CARTAO_CREDITO
+			){
+				error_log('AUTORIZADO');
+				if($pessoa = $this->getRepositorio()->getPessoaORM()->encontrarPorEmail($email)){
+				error_log('ACHOU PESSOA');
+				if($turmaPessoa = $pessoa->getTurmaPessoaAtivo()){
+				error_log('EH ALUNO');
 
-			Funcoes::enviarEmail('falecomleonardopereira@gmail.com', 'teste chekout cielo', 'json post');
+					/* validar qual produto e alterar o financeiro */
+//					$alterarDataDeCriacaoFinanceiro = false;
+//					$cadastroNovo = false;				
+//					$idDisciplina = null;
+//					$disciplina = $this->getRepositorio()->getDisciplinaORM()->encontrarPorId($idDisciplina);
+//					$turmaPessoaFinanceiro = $turmaPessoa->getTurmaPessoaFinanceiroPorDisciplina($disciplina->getId());
+//					if (!$turmaPessoaFinanceiro) {
+//						$turmaPessoaFinanceiro = new TurmaPessoaFinanceiro();	
+//						$cadastroNovo = true;						
+//					}
+//					$turmaPessoaFinanceiro->setDisciplina($disciplina);
+//					$qualAvaliacao = null;
+//					$mes = date('m');
+//					$ano = date('Y');
+//					switch ($qualAvaliacao) {
+//					case 1:
+//						$turmaPessoaFinanceiro->setValor1($valor);
+//						$turmaPessoaFinanceiro->setMes1($mes);
+//						$turmaPessoaFinanceiro->setAno1($ano);
+//						break;
+//					case 2:
+//						$turmaPessoaFinanceiro->setValor2($valor);
+//						$turmaPessoaFinanceiro->setMes2($mes);
+//						$turmaPessoaFinanceiro->setAno2($ano);
+//						break;
+//					case 3:
+//						$turmaPessoaFinanceiro->setValor3($valor);
+//						$turmaPessoaFinanceiro->setMes3($mes);
+//						$turmaPessoaFinanceiro->setAno3($ano);
+//						break;
+//					}
+//					if($cadastroNovo){
+//						$alterarDataDeCriacaoFinanceiro = true;
+//						if(!$turmaPessoaFinanceiro->getValor1()){
+//							$turmaPessoaFinanceiro->setValor1('N');
+//						}
+//						if(!$turmaPessoaFinanceiro->getValor2()){
+//							$turmaPessoaFinanceiro->setValor2('N');
+//						}
+//						if(!$turmaPessoaFinanceiro->getValor3()){
+//							$turmaPessoaFinanceiro->setValor3('N');
+//						}
+//					}					
+				}
+				}
+			}
 		}
 
 		$response->setContent(Json::encode(array('ok' => true,)));
