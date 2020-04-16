83aa63cfede98afda6f48bf63ea88d9ca15b2970
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2020-04-16 19:39:32 -0300

    sincronizacao sete

--- module/Application/src/Application/Controller/LoginController.php
+++ module/Application/src/Application/Controller/LoginController.php
@@ -2314,7 +2314,8 @@ class LoginController extends CircuitoController {
 		$response->getHeaders()
 			->addHeaderLine('Access-Control-Allow-Origin', '*')
 			->addHeaderLine('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-			->addHeaderLine('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+			->addHeaderLine('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+			->addHeaderLine('Content-Type', 'application/json');
 		$response->setContent(Json::encode($dados));
 		return $response;
 	}
