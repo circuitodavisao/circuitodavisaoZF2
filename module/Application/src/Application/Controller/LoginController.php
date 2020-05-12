08b5b9b454bc89186f5a4692d913bcc77af98561
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2020-05-12 14:05:44 -0300

    teste de push notification um

--- module/Application/src/Application/Controller/LoginController.php
+++ module/Application/src/Application/Controller/LoginController.php
@@ -2733,4 +2733,31 @@ return $dados;
 		$response->setContent(Json::encode($dados));
 		return $response;
 	}
+
+	public function pushNotificationAction() {
+		$response = $this->getResponse();
+		$dados = array();
+
+		$pessoa = $this->getRepositorio()->getPessoaORM()->encontrarPorId(1739);
+		$dados['token'] = $pessoa->getToken_notificacao();
+		$dados['title'] = 'Titulo vi aCV';
+		$dados['body'] = 'CORPO DO CV';
+
+		$url = 'https://exp.host/--/api/v2/push/send';
+		$options = array(
+			'http' => array(
+			'header'  => "Content-type: application/json\r\n",
+			'method'  => 'POST',
+			'content' => http_build_query($dados),
+			)
+		);
+
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		var_dump($result);
+
+		$response->setContent(Json::encode($dados));
+		return $response;
+	}
+
 }
