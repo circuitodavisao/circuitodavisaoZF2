a246bca1c91d98ecbf52ae359f4f2c93375b67e2
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2020-06-08 11:25:54 -0300

    atualizacao da implentacao

--- implementarSistema.sh
+++ implementarSistema.sh
@@ -25,14 +25,18 @@ sudo apt-get update
 sudo apt-get install docker-ce=18.06.1~ce~3-0~ubuntu
 
 # instalando sistema
-sudo docker run --name bdpg -e POSTGRES_PASSWORD=qwaszx159753 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -d -v /home/circuitodavisaoZF2/my-postgres.conf:/etc/postgresql/postgresql.conf postgres -c 'config_file=/etc/postgresql/postgresql.conf'
+# PRODUCAO
+# sudo docker run --name bdpg -e POSTGRES_PASSWORD=qwaszx159753 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -d -v /home/circuitodavisaoZF2/my-postgres.conf:/etc/postgresql/postgresql.conf postgres -c 'config_file=/etc/postgresql/postgresql.conf'
+# HOMOLOGACAO
+sudo docker run --name bdpg -e POSTGRES_PASSWORD=qwaszx159753 -e POSTGRES_DB=postgres -e POSTGRES_USER=postgres -d postgres
 
 sudo cd /home/circuitodavisaoZF2
+sudo chmod -R 777 data/
 
 sudo docker build -t cv:0.1 .
 sudo docker run -d --name cv --link bdpg:postgres -p 443:443 -v /home/circuitodavisaoZF2:/var/www/html cv:0.1
 
 # instalando o banco de dados
-sudo scp root@51.89.96.128:/home/backup/dump /home/dump
+# sudo scp root@51.89.96.128:/home/backup/dump /home/dump
 sudo tar -xzvf dump
 sudo docker exec -i bdpg psql -U postgres < /home/dump.sql
--- iniciarDocker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker start bdpg
-docker rm cv
-docker run -d --name cv --link bdpg:postgres -p 443:443 -v /home/developer/Documents/circuitodavisaoZF2:/var/www/html cv:0.5
