94ab0e80aabc136a941a8e0b7b05ba37d007b0af
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2021-04-09 17:56:55 -0300

    cv aluno

--- module/Application/src/Application/Controller/LoginController.php
+++ module/Application/src/Application/Controller/LoginController.php
@@ -1119,6 +1119,7 @@ class LoginController extends CircuitoController {
 					if(
 						$turmaPessoa->getTurma()->getGrupo()->getGrupoRegiao()->getId() === 3110 // REGIAO DF
 
+						|| $turmaPessoa->getTurma()->getGrupo()->getGrupoIgreja()->getId() === 4161 
 						|| $turmaPessoa->getTurma()->getGrupo()->getGrupoIgreja()->getId() === 9496 
 						|| $turmaPessoa->getTurma()->getGrupo()->getGrupoIgreja()->getId() === 3262  
 						|| $turmaPessoa->getTurma()->getGrupo()->getGrupoIgreja()->getId() === 30371
