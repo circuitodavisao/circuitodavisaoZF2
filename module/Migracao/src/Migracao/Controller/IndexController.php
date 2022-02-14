6b32872a16d99b50200957ce1ab86002517cf6ac
leonardo pereira magalhaes <falecomleonardopereira@gmail.com>
2022-02-14 19:04:23 -0300

    rorar 8

--- module/Migracao/src/Migracao/Controller/IndexController.php
+++ module/Migracao/src/Migracao/Controller/IndexController.php
@@ -458,7 +458,7 @@ class IndexController extends CircuitoController {
 			$this->getRepositorio()->iniciarTransacao();
 			try {
 				//for($indiceHierarquia = 10; $indiceHierarquia > 0; $indiceHierarquia--){
-					$indiceHierarquia = 9;
+					$indiceHierarquia = 8;
 					$html .= '<br /><br /><br />Hierarquia: '.$indiceHierarquia;
 					if($solicitacoesPorHierarquia[$indiceHierarquia]){
 						$html .= '<br />TEM SOLICITACOES';
